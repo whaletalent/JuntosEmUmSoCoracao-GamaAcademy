b708dab20b3c4dd21468a8f2aa19cea81ea2c043
LuanFerreira <luanfc@gmail.com>
2019-07-02 16:14:10 -0300

    Adicionados: NavMenu e HeroSection

---
+++ src/components/HeroSection.js
@@ -0,0 +1,44 @@
+import React, {Component, Fragment} from 'react'
+import {Hero, Container, Title } from 'rbx'
+import NavMenu from './NavMenu'
+
+export default class HeroSection extends Component{
+    render(){
+        return(
+            <Fragment>
+                
+                <Hero size="fullheight" color="default-blue">
+                    {/* Hero head: will stick at the top */}
+                    <Hero.Head>
+                        <NavMenu/>
+                    </Hero.Head>
+                    {/* Hero content: will be in the middle */}
+                    <Hero.Body>
+                    <Container textAlign="centered">
+                        <Title size={1}>Apoie Nossa Causa</Title>
+                        {/* <Title as="h2" subtitle>
+                        Subtitle
+                        </Title> */}
+                    </Container>
+                    </Hero.Body>
+                
+                    {/* Hero footer: Inserir caso haja necessidade */}
+                    {/* <Hero.Foot>
+                    <Container>
+                        <Tab.Group as="nav" type="boxed" fullwidth>
+                        <Tab active>Seja um Voluntário</Tab>
+                        <Tab>Doações</Tab>
+                        <Tab>Seja um patrocinador</Tab>
+                        <Tab>Disk Help</Tab>
+                        <Tab>Ações Voluntárias</Tab>
+                        <Tab>Galeria</Tab>
+                        <Tab>Contato</Tab>
+                        </Tab.Group>
+                    </Container>
+                    </Hero.Foot> */}
+                </Hero>
+                
+            </Fragment>
+        )
+    }
+}
---
+++ src/components/NavMenu.js
@@ -0,0 +1,51 @@
+import React, {Component, Fragment} from 'react'
+import {Navbar, Container, Button} from 'rbx'
+
+export default class NavMenu extends Component{
+    state={
+        menuItems: [
+            'Quero ser um voluntário',
+            'Seja um patrocinador',
+            'Disk Help',
+            'Ações Voluntárias',
+            'Galeria',
+            'Contato',
+        ]
+    }
+    render(){
+        return(
+            <Fragment>
+
+                <Navbar>
+                <Container>
+                    <Navbar.Brand>
+                    <Navbar.Item>
+                        <img
+                        src="https://www.qbrobotics.com/wp-content/uploads/2018/03/sample-logo.png"
+                        alt="Logo"
+                        />
+                    </Navbar.Item>
+                    <Navbar.Burger />
+                    </Navbar.Brand>
+                    <Navbar.Menu>
+                    <Navbar.Segment align="end">
+                        {
+                            this.state.menuItems.map( item =>{
+                                return(
+                                    <Navbar.Item active>{item}</Navbar.Item>
+                                )
+                            })
+                        }
+                        <Navbar.Item as="span">
+                        <Button color="primary" inverted>
+                            <span>Doar</span>
+                        </Button>
+                        </Navbar.Item>
+                    </Navbar.Segment>
+                    </Navbar.Menu>
+                </Container>
+                </Navbar>;
+            </Fragment>
+        )
+    }
+}
